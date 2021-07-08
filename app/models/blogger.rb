be307cc5ce9830cd05e86a7ef0f7650173cc8602
Lindsay <lindsay.chen@yale.edu>
2021-07-09 04:17:18 +0800

    mock challenge

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,19 @@
 class Blogger < ApplicationRecord
+    has_many :posts
+    has_many :destinations, through: :posts
+    validates :name, uniqueness: true
+    validates :bio, length: { minimum: 30 }
+    validates :age, numericality: { greater_than: 0 }
 
+    # def total_likes
+    #     all_likes = self.posts.map{|post| post.likes}
+    #     all_likes.sum
+    # end
+
+    def featured_post
+        # self.posts.max_by{|post| post.likes}
+        self.posts.order(likes: :desc)[0]
+    end
+
+    
 end
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,19 @@
 class Destination < ApplicationRecord
+    has_many :posts
+    has_many :bloggers, through: :posts
+
+    def recent
+        self.posts.last(5)
+    end
+
+    def featured_post
+        self.posts.max_by{|post| post.likes}
+    end
+
+    def ave_age
+        bl = self.bloggers.uniq
+        ave = bl.map{|blogger| blogger.age}
+        ave.sum / ave.count
+    end
 
 end
